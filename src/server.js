806636ab97044f077bdf02c920570ce61c4ab04e
AdapterCo <123659426+AdapterCo@users.noreply.github.com>
2023-12-31 14:33:09 -0300

    make it better

--- src/server.js
+++ src/server.js
@@ -18,36 +18,12 @@ const reqGNAlready = GNRequest({
   clientSecret: process.env.GN_CLIENT_SECRET
 });
 
-app.get('/', async (req, res) => {
-  const reqGN = await reqGNAlready;
-  const dataCob = {
-    calendario: {
-      expiracao: 3600
-    },
-    valor: {
-      original: '0.10'
-    },
-    chave: '126bec4a-2eb6-4b79-a045-78db68412899',
-    solicitacaoPagador: 'Cobrança dos serviços prestados.'
-  };
-
-  const cobResponse = await reqGN.post('/v2/cob', dataCob);
-  const qrcodeResponse = await reqGN.get(`/v2/loc/${cobResponse.data.loc.id}/qrcode`);
-
-  res.render('qrcode', { qrcodeImage: qrcodeResponse.data.imagemQrcode })
-});
-
 app.get('/cobrancas', async(req, res) => {
   const reqGN = await reqGNAlready;
 
   const cobResponse = await reqGN.get('/v2/pix?inicio=2023-12-01T16:01:35Z&fim=2023-12-31T23:59:00Z');
 
   res.send(cobResponse.data);
-});
-
-app.post('/webhook(/pix)?', (req, res) => {
-  console.log(req.body);
-  res.send('200');
 });
 
 app.listen(8000, () => {
