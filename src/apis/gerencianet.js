52b849e28dba81fc759369a42aa5d4fda19062aa
AdapterCo <123659426+AdapterCo@users.noreply.github.com>
2024-01-01 00:10:58 -0300

    Teste aws

--- src/apis/gerencianet.js
+++ src/apis/gerencianet.js
@@ -34,7 +34,7 @@ const authenticate = ({ clientID, clientSecret }) => {
 
 const GNRequest = async (credentials) => {
   const authResponse = await authenticate(credentials);
-  const accessToken = authResponse.data?.access_token;
+  const accessToken = authResponse.data.access_token;
 
   return axios.create({
     baseURL: process.env.GN_ENDPOINT,
